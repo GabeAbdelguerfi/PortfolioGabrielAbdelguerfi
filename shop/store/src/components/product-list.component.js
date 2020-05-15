afbc2b4533fb83dea7faac8cbe6893ed29d5183f
GabeAbdelguerfi <Gaabdelg@uno.edu>
2020-05-14 22:21:15 -0500

    search feature

--- shop/store/src/components/product-list.component.js
+++ shop/store/src/components/product-list.component.js
@@ -1,7 +1,8 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useEffect } from 'react'
 import Axios from 'axios';
+import { Input } from 'antd';
 import '../App.css';
-import SearchFeature from './SearchFeature';
+const { Search } = Input;
 
 export default class ProductList extends Component {
 
@@ -9,12 +10,16 @@ export default class ProductList extends Component {
         itemname: '',
         price: '',
         imageUrl: '',
-        products: []
+        products: [],
+        searchTerm: '',
+        matchingSearch: false,
+        productSearch: []
 
     }
 
     componentDidMount = () =>{
         this.getProductList();
+        this.displayProducts(this.state.products);
     };
 
     getProductList = () => {
@@ -28,12 +33,31 @@ export default class ProductList extends Component {
         });
     }
 
+    onChangeSearch = (event) => {
+        this.setState({searchTerm: event.currentTarget.value})
+       // console.log(this.state.searchTerm)
+        let i = 0;
+        let products = this.state.products;
+        // console.log(products);
+        while(i < products.length){
+            if(products[i].itemname === event.currentTarget.value){
+            this.setState({productSearch: products[i]})
+            this.setState({matchingSearch: true})
+            }
+        i++;
+        }
+    }
+
     displayProducts = (products) => {
-
-        if(!products.length) return null;
-
+        //console.log(this.state.searchTerm)
+        console.log(this.state.productSearch)
+        let searchResult = this.state.productSearch;
+        if(this.state.searchTerm == ''){
+            
         return products.map((product, index) => (
+
             <div className="shop-item container col col-6 md-col-12" key={index}>
+                {console.log(index)}
                 <div className='container inner-element'>
                     <h3 className="shop-item">{product.itemname}</h3>
                     <img className="item-image" src={product.imageUrl} width="130px" height="130px" alt="..."></img>
@@ -41,39 +65,68 @@ export default class ProductList extends Component {
                     <a href={`/detail/${this.props.match.params.user}/${product._id}`}>More Information</a>
                     <button className="btn add-cart-button" onClick={() => {
                         console.log('clicked')
+                        this.setState(state => ({
+                            product: [product.cart, product]
+                        }))
+                        console.log(this.state)
                         Axios.post('http://localhost:5000/users/add-to-cart', {
                                 user: this.props.match.params.user,
                                 p_id: product._id 
                             }).then ((res) => {
                                 console.log('testing')
+                                console.log(product._id)
                             }).catch((res) => {
                                 console.log(res)
                             })
                     }} type="button">ADD TO CART</button>
                 </div>
             </div>
-    ));
+            ));
+         }
+         else if(this.state.searchTerm != '' && this.state.matchingSearch == true){
+            // return <div>hi</div>
+            return (
+                <div className="shop-item container col col-6 md-col-12">
+                    <div className='container inner-element'>
+                        <h3 className="shop-item">{searchResult.itemname}</h3>
+                        <img className="item-image" src={searchResult.imageUrl} width="130px" height="130px" alt="..."></img>
+                        <p className="item-price">Price: ${searchResult.price}</p>
+                        <a href={`/detail/${this.props.match.params.user}/${searchResult._id}`}>More Information</a>
+                        <button className="btn add-cart-button" onClick={() => {
+                            console.log('clicked')
+                            console.log(this.state)
+                            Axios.post('http://localhost:5000/users/add-to-cart', {
+                                    user: this.props.match.params.user,
+                                    p_id: searchResult._id 
+                                }).then ((res) => {
+                                    console.log('testing')
+                                    console.log(searchResult._id)
+                                }).catch((res) => {
+                                    console.log(res)
+                                })
+                        }} type="button">ADD TO CART</button>
+                    </div>
+                </div>
+                );
+          }
     }
-    searchFeature = () => {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <SearchFeature
-                    // refreshFunction={updateSearchTerms}
-                />
-            </div>
-    )}
 
-    updateSearchTerms = () =>{
-
-    }
 
     render() {
+        // console.log(this.searchTerm.)
         return (
-            <div>
-                {this.searchFeature()}
-                <div className='row'>
-                    {this.displayProducts(this.state.products)}
-                </div>
+                <div>
+                    <div>
+                    <Search
+                        value={this.searchTerms}
+                        onChange={this.onChangeSearch}
+                        onClick={this.onChangeSearch}
+                        placeholder="Search by typing..."
+                        />
+                    </div>
+                        <div className='row'>
+                            {this.displayProducts(this.state.products)}
+                        </div>
             </div>
         )
     }
